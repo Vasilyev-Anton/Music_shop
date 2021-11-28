36e9d2abf831a4c71e08c87839a055347cec6e88
Anton Vasilyev <vasilyev1812@gmail.com>
2021-11-28 12:15:25 +0300

    added files

---
+++ singer_202111281156.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.singer (name_singer) VALUES
+	 ('Pink'),
+	 ('The Beatles'),
+	 ('Led Zeppelin'),
+	 ('Scorpions'),
+	 ('Madonna'),
+	 ('Queen'),
+	 ('Bon Jovi'),
+	 ('Linkin Park');
---
+++ track_202111281156.sql
@@ -0,0 +1,19 @@
+INSERT INTO public.track (title,album_id,duration,duration1) VALUES
+	 ('Yesterday',8,'00:02:03',2.03),
+	 ('Help!',9,'00:02:18',2.18),
+	 ('Numb',11,'00:03:07',3.07),
+	 ('Breaking the Habit',11,'00:03:15',3.15),
+	 ('I Want to Break Free',5,'00:03:20',3.20),
+	 ('It''s My Life',10,'00:03:45',3.45),
+	 ('Just Like a Pill',6,'00:03:57',3.57),
+	 ('It''s a Hard Life',5,'00:04:09',4.09),
+	 ('Rock You Like a Hurricane',2,'00:04:11',4.11),
+	 ('You''ll See',5,'00:04:39',4.39);
+INSERT INTO public.track (title,album_id,duration,duration1) VALUES
+	 ('I Want It All',4,'00:04:40',4.40),
+	 ('Family Portrait',6,'00:04:56',4.56),
+	 ('The Miracle',4,'00:05:06',5.06),
+	 ('The Rover',1,'00:05:36',5.36),
+	 ('Live to Tell',5,'00:05:52',5.52),
+	 ('Holiday',3,'00:06:08',6.08),
+	 ('Everybody',3,'00:06:43',6.43);
